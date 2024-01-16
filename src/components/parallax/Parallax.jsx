baac6dc77b6c4611ee7e8c36aee4402e7515dddc
auronvila <auronvila1121@gmail.com>
2024-01-16 23:28:37 +0300

    update image paths

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -19,7 +19,7 @@ export default function Parallax({type}) {
       style={{background: type === 'services' ? 'linear-gradient(180deg, #111132, #0c0c1d)' : 'linear-gradient(180deg, #111132, #505064)'}}>
       <motion.h1 style={{y: yText}}>{type === 'services' ? 'What we do' : 'What we did'}</motion.h1>
       <motion.div className={'mountains'}></motion.div>
-      <motion.div style={{y: yBb, backgroundImage: `url(${type === 'services' ? '/planets.png' : 'sun.png'})`}}
+      <motion.div style={{y: yBb, backgroundImage: `url(${type === 'services' ? 'planets.png' : 'sun.png'})`}}
                   className={'planets'}></motion.div>
       <motion.div style={{x: xBb}} className={'stars'}></motion.div>
     </div>
